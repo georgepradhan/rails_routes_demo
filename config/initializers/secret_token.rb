3e60f18b2bf34a7721927c2dce3032045d95e6dc
George Pradhan <kgeorgepradhan@gmail.com>
2013-09-16 13:59:41 -0700

    first commit. "rails new" files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRoutesDemo::Application.config.secret_token = '591e4ed7b14cf1e40dd9b2b89e799838f0a3ff0d9e6e9b51544e99be4e944d60f6ed26977c9083caf8a4ba5a824c64b7e4366e41238b9fe753046c62c00d895a'
